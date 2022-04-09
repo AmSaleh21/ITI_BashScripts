0ed007625eff86c29ca216201e629004254f04fc
Private <msaleh.abdelrahman@gmail.com>
2022-04-09 15:04:50 +0200

    useless cat removed

--- Lab_1/addData.sh
+++ Lab_1/addData.sh
@@ -5,8 +5,8 @@ DATABASE="BashLabOne"
 T1="Invoices"
 T2="Invoices_details"
 
-cat invoice-data.txt | awk -v USR=${USR} -v DATABASE=${DATABASE} -v T1=${T1} -f "Invoice.awk" |mysql -u ${USR}
-cat product-data.txt | awk -v USR=${USR} -v DATABASE=${DATABASE} -v T2=${T2} -f "product.awk" |mysql -u ${USR}
+< invoice-data.txt awk -v USR=${USR} -v DATABASE=${DATABASE} -v T1=${T1} -f "Invoice.awk" |mysql -u ${USR}
+< product-data.txt awk -v USR=${USR} -v DATABASE=${DATABASE} -v T2=${T2} -f "product.awk" |mysql -u ${USR}
 
 
 exit 0
