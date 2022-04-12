ac10cf3a71d201ca67689071377cad7b9711b20a
Private <msaleh.abdelrahman@gmail.com>
2022-04-12 18:04:10 +0200

    made the IP variable

--- Lab_2/installApacheRemote.sh
+++ Lab_2/installApacheRemote.sh
@@ -6,7 +6,7 @@
 #   1: connection failed
 #   2: file not found
 #   3: file read failed
-#   4: data bad
+#   4: bad data sent
 
 DATAFILE='./data/VMconnection'
 
@@ -18,16 +18,18 @@ DATAFILE='./data/VMconnection'
 ## get the data from the file
 USER=$(< ${DATAFILE} cut -d":" -f 2 | sed -n 1p)
 PASS=$(< ${DATAFILE} cut -d":" -f 2 | sed -n 2p)
+IP=$(< ${DATAFILE} cut -d":" -f 2 | sed -n 3p)
 
 # check if the data files recieved correctly
-[ -z "${USER}" ] && echo 'file data bad, exit 4' && exit 4
-[ -z "${PASS}" ] && echo 'file data bad, exit 4' && exit 4
+[ -z "${USER}" ] || [ "${USER}" == "<guest user name>" ] && echo 'file data bad, exit 4' ; exit 4
+[ -z "${PASS}" ] || [ "${PASS}" == "<guest password>" ] &&  echo 'file data bad, exit 4' ; exit 4
+[ -z "${IP}" ] || [ "${IP}" == "<guestIP>" ] && echo 'bad data sent in the file' ; exit 4
 
 # install and enable apache2
 printf "installing apache ... \n"
-ssh "${USER}"@192.168.9.129 "echo ${PASS} | sudo -S apt install apache2"
+ssh "${USER}"@"${IP}" "echo ${PASS} | sudo -S apt install apache2"
 printf "apache installed \nenabling\n"
-ssh "${USER}"@192.168.9.129 "echo ${PASS} | sudo -S service apache2 start"
+ssh "${USER}"@"${IP}" "echo ${PASS} | sudo -S service apache2 start"
 printf "\n Done, Bye"
 
 exit 0
